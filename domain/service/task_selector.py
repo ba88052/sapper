84c4f6361bc85fa389560fd1610e4dbc7881df6d
Sam_Liao <ba88052@gmail.com>
2023-12-06 09:07:37 +0800

    更改 task_selector 的條件

--- domain/service/task_selector.py
+++ domain/service/task_selector.py
@@ -1,11 +1,12 @@
 
-from domain.service.task.customize_select_task import CallSeonFraudApiTask
+from domain.service.task.customize_select_task import CustomizeSelect
+from domain.service.task.flatten_json_task import FlattenJson
 
 class TaskSelectorDomainService:
     def select(self, task_name, mission_id, mission_name, domain_infra_respository):
-        if task_name == "call_seon_fraud_api":
-            return CallSeonFraudApiTask(mission_id=mission_id, mission_name=mission_name, domain_infra_respository=domain_infra_respository)
-        # elif mission == "gdt_individual_business_financial_spider":
-            # return GdtIndividualBusinessFinancialCleaner()
+        if task_name == "flatten_json":
+            return FlattenJson(mission_id=mission_id, mission_name=mission_name, domain_infra_respository=domain_infra_respository)
+        elif task_name == "customize_select":
+            return CustomizeSelect(mission_id=mission_id, mission_name=mission_name, domain_infra_respository=domain_infra_respository)
         else:
             raise ValueError(f"Mission is not defind.")
