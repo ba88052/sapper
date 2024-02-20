2f2d17603c71fcf5139cdcf92d6984265ddb8a04
sam.liaw <sam.liaw@cathayholdings.com.tw>
2024-02-20 11:36:58 +0800

    加入config handle 機制

--- infrastructure/config_handler.py
+++ infrastructure/config_handler.py
@@ -8,15 +8,18 @@ def load_infra_config():
     """
     with open("./sapper_infra_config.json", "r") as file:
         return json.load(file)
-
+    
+INFRA_CONFIG = load_infra_config()
 
 def load_monitoring_config():
     """
     專門用來讀取mission process config檔案
     """
-    with open("./monitoring_config.json", "r") as file:
-        return json.load(file)
-
-
-INFRA_CONFIG = load_infra_config()
+    dataset_name = INFRA_CONFIG["bigquery"]["config"]["dataset_name"]
+    monitoring_parameter_table_name = INFRA_CONFIG["bigquery"]["config"]["monitoring_parameter_table_name"]
+    table_path = f"{dataset_name}.{monitoring_parameter_table_name}"
+    config = INFRA_CONFIG(table_path).extract_last_config("PARAMETER")
+    return config
+    # with open("./monitoring_config.json", "r") as file:
+    #     return json.load(file)
 MONITORING_CONFIG = load_monitoring_config()
