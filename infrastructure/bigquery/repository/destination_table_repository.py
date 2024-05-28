c057be04f82fa95a9bd9160da08e49383ff2f619
sam.liaw <sam.liaw@cathayholdings.com.tw>
2024-05-28 16:32:54 +0800

    [Enhancement] 讓非TMP_GENERAL Tablle 也包含 UUID、Job_ID、Job_Name 等資訊

--- infrastructure/bigquery/repository/destination_table_repository.py
+++ infrastructure/bigquery/repository/destination_table_repository.py
@@ -12,29 +12,29 @@ class DestinationTableRepository(BqClient):
 
     def __init__(self, destination_table_path):
         super().__init__()
-        # self.bigquery_table_id = f"{self.project}.{self.dataset}.{self.table}"
         self.bigquery_table_id = destination_table_path
         self.table_schema = self.__get_table_schema(self.bigquery_table_id)
 
-    def save(self, general_tmp_data_entity_list, use_tmp_table):
+    def save(self, general_tmp_data_entity_list, use_tmp_table: bool):
         """
         把 general_tmp_data_entity 存到bq
 
         Args:
-            general_tmp_data_entity (general_tmp_data_entity實體)
-            use_tmp_table(Booling):是否使用暫存表，使用表示還有下一個動作
+            general_tmp_data_entity_list (list): general_tmp_data_entity實體的列表
+            use_tmp_table (bool): 是否使用暫存表，使用表示還有下一個動作
         """
         data_to_insert = []
-
         for general_tmp_data_entity in general_tmp_data_entity_list:
-            if use_tmp_table == True:
-                # 转换格式并收集待插入的数据
+            if use_tmp_table:
                 general_tmp_data_dict = self.__convert_to_tmp_table_format(general_tmp_data_entity)
                 data_to_insert.append(general_tmp_data_dict)
             else:
-                # 优化点：批量处理 TMP_DATA，减少插入次数
+                UUID_Request = general_tmp_data_entity.MISSION_ID
+                MISSION_NAME = general_tmp_data_entity.MISSION_NAME
+                JOB_NAME = general_tmp_data_entity.JOB_NAME
+                JOB_ID = general_tmp_data_entity.JOB_ID
                 for destination_data in general_tmp_data_entity.TMP_DATA:
-                    destination_data_dict = self.__convert_to_destination_table_format(destination_data)
+                    destination_data_dict = self.__convert_to_destination_table_format(destination_data, UUID_Request, MISSION_NAME, JOB_NAME, JOB_ID)
                     data_to_insert.append(destination_data_dict)
 
         insertion_errors = self.client.insert_rows_json(self.bigquery_table_id, data_to_insert)
@@ -43,40 +43,9 @@ class DestinationTableRepository(BqClient):
         else:
             print("Data stored successfully to BigQuery.")
 
-        # if use_tmp_table == True:
-        #     general_tmp_data_dict = self.__convert_to_tmp_table_format(
-        #         general_tmp_data_entity
-        #     )
-        #     insertion_errors = self.client.insert_rows_json(
-        #         self.bigquery_table_id, [general_tmp_data_dict]
-        #     )
-        #     if insertion_errors:
-        #         print(
-        #             f"Errors occurred while storing destination_data_dict to BigQuery: {insertion_errors}"
-        #         )
-        #     else:
-        #         print("destination_data_dict stored successfully to BigQuery.")
-        # else:
-        #     for destination_data in general_tmp_data_entity.TMP_DATA:
-        #         print("destination_data", destination_data)
-        #         print("destination_data_type", type(destination_data))
-        #         destination_data_dict = self.__convert_to_destination_table_format(
-        #             table_id=self.bigquery_table_id, destination_data=destination_data
-        #         )
-        #         insertion_errors = self.client.insert_rows_json(
-        #             self.bigquery_table_id, [destination_data_dict]
-        #         )
-
-        #         if insertion_errors:
-        #             print(
-        #                 f"Errors occurred while storing destination_data_dict to BigQuery: {insertion_errors}"
-        #             )
-        #         else:
-        #             print("destination_data_dict stored successfully to BigQuery.")
-
     def __get_table_schema(self, table_id):
         """
-        # 獲取 BigQuery 表的 schema
+        獲取 BigQuery 表的 schema
 
         Args:
             table_id (str): 目標表的ID，或者說路徑
@@ -84,58 +53,59 @@ class DestinationTableRepository(BqClient):
         Returns:
             dict: 目標表的schema dict
         """
-
         table = self.client.get_table(table_id)
         schema_field_names = {field.name for field in table.schema}
-        # print("schema", schema_field_names)
         return schema_field_names
 
-    def __convert_to_destination_table_format(self, destination_data):
+    def __convert_to_destination_table_format(self, destination_data, UUID_Request, MISSION_NAME, JOB_NAME, JOB_ID):
         """
-        # 比對 JSON 資料與 schema，填充缺失的欄位
+        比對 JSON 資料與 schema，填充缺失的欄位
 
         Args:
-            table_id (str): 目標表的ID，或者說路徑
             destination_data (dict): 要儲存的data
+            UUID_Request (str): 任務ID
+            MISSION_NAME (str): 任務名稱
+            JOB_NAME (str): 工作名稱
+            JOB_ID (str): 工作ID
 
         Returns:
             dict: 整理後的資料
         """
-
         bq_created_time = datetime.now()
         bq_created_time_str = bq_created_time.strftime("%Y-%m-%dT%H:%M:%S.%f")
         bq_updated_time_str = bq_created_time.strftime("%Y-%m-%dT%H:%M:%S.%f")
         partition_date = bq_created_time.strftime("%Y-%m-%d")
+
         destination_data["BQ_CREATED_TIME"] = bq_created_time_str
         destination_data["BQ_UPDATED_TIME"] = bq_updated_time_str
         destination_data["PARTITION_DATE"] = partition_date
-        filled_data = {}
-        for field in self.table_schema:
-            # 如果該欄位在 destination_data 中存在，則使用其值，否則使用預設值 ""
-            filled_data[field] = destination_data.get(field, "")
-        print(filled_data)
+        destination_data["UUID_Request"] = UUID_Request
+        destination_data["MISSION_NAME"] = MISSION_NAME
+        destination_data["JOB_NAME"] = JOB_NAME
+        destination_data["JOB_ID"] = JOB_ID
+
+        filled_data = {field: destination_data.get(field, "") for field in self.table_schema}
         return filled_data
 
     def __convert_to_tmp_table_format(self, general_tmp_data_entity):
         """
-        把 destination_data_dict 轉換成可以存入tmp表的格式
+        把 general_tmp_data_entity 轉換成可以存入tmp表的格式
 
         Args:
-            destination_data_dict (dict)
+            general_tmp_data_entity: general_tmp_data_entity 實體
 
         Returns:
             dict: 可存入tmp表的格式
         """
-        # 將 datetime 對象轉換為 RFC 3339 格式的字符串
         bq_created_time = datetime.now()
         bq_created_time_utc = bq_created_time.replace(tzinfo=timezone.utc)
         bq_created_time_str = bq_created_time_utc.isoformat()
-        bq_created_time_str = bq_created_time.strftime("%Y-%m-%dT%H:%M:%S.%f")
         bq_updated_time_str = bq_created_time.strftime("%Y-%m-%dT%H:%M:%S.%f")
         partition_date = bq_created_time.strftime("%Y-%m-%d")
+
         general_tmp_data_entity.BQ_CREATED_TIME = bq_created_time_str
         general_tmp_data_entity.BQ_UPDATED_TIME = bq_updated_time_str
         general_tmp_data_entity.PARTITION_DATE = partition_date
-        # 把class轉dict
+
         bq_dict = vars(general_tmp_data_entity)
         return bq_dict
